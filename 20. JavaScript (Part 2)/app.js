29b79efab4de082b11d2cae47e9b695514ec7d05
Pulkit <kpulkit15234@gmail.com>
2023-07-11 22:28:21 +0530

    operators in js [ lec-20 ]

--- 20. JavaScript (Part 2)/app.js
+++ 20. JavaScript (Part 2)/app.js
@@ -1,6 +1,24 @@
-// console.log('hello')
+// console.log('hello');
 
-let pencliPrice = 10;
-let eraserPrice = 5;
-let output = `The total price is ${pencliPrice+eraserPrice} Rupees.`;
-console.log(output);    
+// let pencliPrice = 10;
+// let eraserPrice = 5;
+// let output = `The total price is ${pencliPrice+eraserPrice} Rupees.`;
+// console.log(output);
+
+// let a=10
+// let b=5
+// // Arithmetic Operator
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// // Unary Operator
+// console.log(a++);
+// console.log(++a);
+// Assignment Operator
+let a=10;
+let b=20;
+b=a;
+console.log(b);
