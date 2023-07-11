95f6093007f12a96c8b09e77e2dc4376462e0427
Pulkit <kpulkit15234@gmail.com>
2023-07-11 22:06:04 +0530

    template literals file [ lec-20 ]

--- 20. JavaScript (Part 2)/app.js
+++ 20. JavaScript (Part 2)/app.js
@@ -1,1 +1,6 @@
-console.log('hello')
+// console.log('hello')
+
+let pencliPrice = 10;
+let eraserPrice = 5;
+let output = `The total price is ${pencliPrice+eraserPrice} Rupees.`;
+console.log(output);    
