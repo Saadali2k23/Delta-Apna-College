16f14559ac22cc3f642e2fff999fffc245e1dfd8
Pulkit <kpulkit15234@gmail.com>
2023-09-17 15:42:35 +0530

    JS part 20

--- 20. JavaScript (Part 2)/app.js
+++ 20. JavaScript (Part 2)/app.js
@@ -1,50 +1,15 @@
-// console.log('hello');
-
-// let pencliPrice = 10;
-// let eraserPrice = 5;
-// let output = `The total price is ${pencliPrice+eraserPrice} Rupees.`;
-// console.log(output);
-
-// let a=10
-// let b=5
-// // Arithmetic Operator
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b);
-// console.log(a**b);
-// // Unary Operator
-// console.log(a++);
-// console.log(++a);
-// Assignment Operator
-// let a=10;
-// let b=20;
-// b=a;
-// console.log(b);
-
-// // comparison operator
-// let age = 18;
-// console.log(age<18);
-
-// // Conditional Statement
-// console.log("before my if statement");
-
-// let age = 23;
-// if (age>=18) {
-//     console.log("you can vote");
+// console.log("Welcome to the most addictive game")
+// number = Math.floor(Math.random()*10)
+// guess = prompt("Enter a number")
+// while(true){
+//     if (number == guess) {
+//         console.log("You Won the number was "+number)
+//         break;
+//     }
+//     else if (guess == "quit") {
+//         console.log("See you soon , the number was "+number)
+//         break;
+//     }else{
+//         guess = prompt("Wrong guess , try again")
+//     }
 // }
-
-// console.log("after my if statement");
-
-// truthy and falsy values
-
-// let a = '';
-
-// if (a){
-//     console.log(true);
-// }else{
-//     console.log(false);
-// }
-
-alert("your name is " + prompt("Enter your age "))
