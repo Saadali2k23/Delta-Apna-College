69c88285ae86c6c08c7cd36e8330a17c501d7ed6
Pulkit <kpulkit15234@gmail.com>
2023-09-21 17:12:47 +0530

    User routes using sql

--- 40 SQL with Node/index.js
+++ 40 SQL with Node/index.js
@@ -1,7 +1,20 @@
-const { faker, da } = require('@faker-js/faker');
-const mysql = require("mysql2")
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require('method-override')
 
-const createRandomUser = () => {
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs')
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")));
+app.use(methodOverride('_method'))
+
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2")
+const sql = require('./sql');
+const { connect } = require("http2");
+ 
+const getRandomUser = () => {
     return [
       faker.string.uuid(),
       faker.internet.userName(),
@@ -9,98 +22,74 @@ const createRandomUser = () => {
       faker.internet.password(),
     ];
 }
-const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    database:"delta_app",
-    password:"root"
+
+app.get("/", (req, res) => {
+    const connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        database:"delta_app",
+        password:"root"
+    })
+    sql.runCommand(connection, "SELECT count(*) FROM USER", ( err,results) => {
+        const users = `${results[0]['count(*)']}`
+        res.render("index",{users});
+    });
+});
+
+app.get("/users", (req, res) => {
+    const connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        database:"delta_app",
+        password:"root"
+    })
+    // const data = []
+    // for (let i=0;i<100;i++){
+    //     data.push(getRandomUser())
+    // }
+
+    // sql.Delete(connection)
+    // sql.insertMany(connection,data)
+    sql.runCommand(connection, "SELECT * FROM USER", ( err,results) => {
+        results.forEach(res => {
+            delete(res.password)
+        });
+        res.render("users",{users:results});
+    });
+});
+
+app.get("/users/:id/edit", (req, res) => {
+    res.render("edit",{id:req.params.id});
+});
+
+app.patch("/users/:id", (req, res) => {
+    const connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        database:"delta_app",
+        password:"root"
+    })
+    const id = req.params.id;
+    try{
+        connection.query(
+            `SELECT password FROM user where id='${id}'`,
+            (err,results)=>{
+                if(err) throw err;
+                if (results[0].password == req.body.password ){
+                    sql.updateUsername(connection, req.params.id ,req.body.username,req.body.password);
+                    res.redirect("/users")
+                }else{
+                    res.send("Invalid Password")
+                }
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+    
+});
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`app is listening at port ${PORT}`);
 })
-
-const runCommand = (query,values) => {
-    try{
-        values?
-        connection.query(
-            query,values,
-            (err,results)=>{
-                if(err) throw err;
-                console.log(results);
-            }
-        )
-        :
-        connection.query(
-            query,
-            (err,results)=>{
-                if(err) throw err;
-                console.log(results);
-            }
-        )
-    }catch(err){
-        console.log(err);
-    }
-}
-
-const insert = (value) => {
-    try{
-        connection.query(
-            "INSERT INTO user VALUES(?,?,?,?)",value,
-            (err,results)=>{
-                if(err) throw err;
-                console.log("record added successfully");
-            }
-        )
-    }catch(err){
-        console.log(err);
-    }
-}
-                console.log("All records deleted successfully");
-const insertMany = (values) => {
-    try{
-        connection.query(
-            "INSERT INTO user VALUES ?",[values],
-            (err,results)=>{
-                if(err) throw err;
-                console.log("All records added successfully");
-            }
-        )
-    }catch(err){
-        console.log(err);
-    }
-}
-
-const select = () => {
-    try{
-        connection.query(
-            "SELECT * FROM user",
-            (err,results)=>{
-                if(err) throw err;
-                console.log(results);
-            }
-        )
-    }catch(err){
-        console.log(err);
-    }
-}
-
-const Delete = () => {
-    try{
-        connection.query(
-            "DELETE FROM user",
-            (err,results)=>{
-                if(err) throw err;
-                console.log("All records deleted successfully");
-            }
-        )
-    }catch(err){
-        console.log(err);
-    }
-}
-
-const data = []
-for (let i=0;i<2000;i++){
-    data.push(createRandomUser())
-}
-
-Delete();
-insertMany(data)
-
-connection.end()
---
+++ 40 SQL with Node/sql.js
@@ -0,0 +1,94 @@
+const runCommand = (connection,query,values) => {
+    try{
+        values?
+        connection.query(
+            query,values,
+            (err,results)=>{
+                if(err) throw err;
+                return results;
+            }
+        )
+        :
+        connection.query(
+            query,
+            (err,results)=>{
+                if(err) throw err;
+                return results;
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const insert = (connection,value) => {
+    try{
+        connection.query(
+            "INSERT INTO user VALUES(?,?,?,?)",value,
+            (err,results)=>{
+                if(err) throw err;
+                console.log("record added successfully");
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+}
+const insertMany = (connection,values) => {
+    try{
+        connection.query(
+            "INSERT INTO user VALUES ?",[values],
+            (err,results)=>{
+                if(err) throw err;
+                console.log("All records added successfully");
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const getUsers = (connection) => {
+    try{
+        connection.query(
+            "SELECT * FROM user",
+            (err,results)=>{
+                if(err) throw err;
+                console.log(results);
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const Delete = (connection) => {
+    try{
+        connection.query(
+            "DELETE FROM user",
+            (err,results)=>{
+                if(err) throw err;
+                console.log("All records deleted successfully");
+            }
+        )
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const updateUsername = (connection,id,newusername,password) => {
+    try{
+        runCommand(connection,`update user set username='${newusername}' where id='${id}'`)
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    runCommand,
+    insert,
+    insertMany,
+    getUsers,
+    Delete,
+    updateUsername
+};
